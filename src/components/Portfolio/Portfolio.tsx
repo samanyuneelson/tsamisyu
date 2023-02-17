09bafde51547ab2776a613815d18f09be1fe8bdd
Samanyu <samanyuneelson@gmail.com>
2023-02-17 23:21:18 +0530

    move all css to styled components

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,12 +1,12 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { styled } from "@mui/material/styles";
 
 import js from "../../assets/js.png";
 import html from "../../assets/html.png";
 import go from "../../assets/go.png";
 import python from "../../assets/python.png";
 import cpp from "../../assets/cpp.png";
-
 import typescript from "../../assets/typescript.png";
 import gatsby from "../../assets/gatsby.png";
 import reactjs from "../../assets/reactjs.png";
@@ -22,9 +22,69 @@ import afterEffects from "../../assets/aftereffects.png";
 import graphgl from "../../assets/graphql.png";
 import nextjs from "../../assets/nextjs.png";
 
+const MarqueePortfolio = styled(Box)`
+  ul {
+    list-style-type: none;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    padding-inline-start: 0;
+  }
+
+  .portfolio {
+    background-color: #d9d9d9;
+  }
+
+  .marquee {
+    --gap: 5rem;
+    display: flex;
+    flex-direction: row;
+    overflow: hidden;
+    gap: var(--gap);
+    user-select: none;
+  }
+
+  .marquee__content {
+    gap: var(--gap);
+  }
+
+  .programmingIcon {
+    padding: 4% 2% 2% 2%;
+  }
+
+  .infiniteRun {
+    animation-iteration-count: infinite;
+    animation-duration: 30s;
+    animation-timing-function: linear;
+    animation-fill-mode: forwards;
+    animation-name: scroll;
+  }
+
+  .marquee--hover-pause:hover .marquee__content {
+    animation-play-state: paused;
+  }
+
+  @keyframes scroll {
+    0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(calc(-100% - var(--gap)));
+    }
+  }
+
+  li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+  }
+`;
+
 export default function Portfolio() {
   return (
-    <Box className="portfolio" padding="5%">
+    <MarqueePortfolio className="portfolio" padding="5%">
       <Typography variant="h2">Portfolio</Typography>
       <Box display="flex" flexDirection="row" justifyContent="center">
         <div className="programmingIcon">
@@ -134,6 +194,6 @@ export default function Portfolio() {
           </li>
         </ul>
       </Box>
-    </Box>
+    </MarqueePortfolio>
   );
 }
